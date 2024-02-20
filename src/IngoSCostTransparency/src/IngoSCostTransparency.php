0a38a221c4060db24d218bcbf90c3e2f7fec9775
Ingo Steinke <ingosteinke@gmx.de>
2024-02-20 16:53:57 +0100

    add frontend stub

--- src/IngoSCostTransparency/src/IngoSCostTransparency.php
+++ src/IngoSCostTransparency/src/IngoSCostTransparency.php
@@ -3,9 +3,7 @@
 namespace IngoSCostTransparency;
 
 use Shopware\Core\Framework\Plugin;
-use Shopware\Storefront\Framework\ThemeInterface;
 
 class IngoSCostTransparency extends Plugin
 {
 }
-// TODO don't extend theme interface unless needed
---
+++ src/IngoSCostTransparency/src/Resources/app/storefront/src/main.js
@@ -0,0 +1,5 @@
+import IngoSDemoTogetherTheme from './ingo-s-demo-together-theme/ingo-s-demo-together-theme';
+// Import other plugins if necessary
+// Register your plugin via the existing PluginManager
+const PluginManager = window.PluginManager;
+PluginManager.register('IngoSDemoTogetherTheme', IngoSDemoTogetherTheme);
