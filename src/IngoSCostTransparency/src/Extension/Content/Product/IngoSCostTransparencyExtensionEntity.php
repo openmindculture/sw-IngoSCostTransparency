77c2898668f0ce81d90e0275be0d2cce11f6fec2
Ingo Steinke <ingosteinke@gmx.de>
2024-02-26 20:21:56 +0100

    ensure value range 1..100

--- src/IngoSCostTransparency/src/Extension/Content/Product/IngoSCostTransparencyExtensionEntity.php
+++ src/IngoSCostTransparency/src/Extension/Content/Product/IngoSCostTransparencyExtensionEntity.php
@@ -37,51 +37,64 @@ class IngoSCostTransparencyExtensionEntity extends Entity
 
     public function getCostTransparencyPercentage01(): int
     {
-        return $this->costTransparencyPercentage01;
+        return $this->ensurePercentageRange($this->costTransparencyPercentage01);
     }
 
     public function setCostTransparencyPercentage01(?int $costTransparencyPercentage01): void
     {
-        $this->costTransparencyPercentage01 = $costTransparencyPercentage01;
+        $this->costTransparencyPercentage01 = $this->ensurePercentageRange($costTransparencyPercentage01);
     }
 
     public function getCostTransparencyPercentage02(): int
     {
-        return $this->costTransparencyPercentage02;
+        return $this->ensurePercentageRange($this->costTransparencyPercentage02);
     }
 
     public function setCostTransparencyPercentage02(?int $costTransparencyPercentage02): void
     {
-        $this->costTransparencyPercentage02 = $costTransparencyPercentage02;
+        $this->costTransparencyPercentage02 = $this->ensurePercentageRange($costTransparencyPercentage02);
     }
 
     public function getCostTransparencyPercentage03(): int
     {
-        return $this->costTransparencyPercentage03;
+        return $this->ensurePercentageRange($this->costTransparencyPercentage03);
     }
 
     public function setCostTransparencyPercentage03(?int $costTransparencyPercentage03): void
     {
-        $this->costTransparencyPercentage01 = $costTransparencyPercentage03;
+        $this->costTransparencyPercentage01 = $this->ensurePercentageRange($costTransparencyPercentage03);
     }
 
     public function getCostTransparencyPercentage04(): int
     {
-        return $this->costTransparencyPercentage04;
+        return $this->ensurePercentageRange($this->costTransparencyPercentage04);
     }
 
     public function setCostTransparencyPercentage04(?int $costTransparencyPercentage04): void
     {
-        $this->costTransparencyPercentage04 = $costTransparencyPercentage04;
+        $this->costTransparencyPercentage04 = $this->ensurePercentageRange($costTransparencyPercentage04);
     }
 
     public function getCostTransparencyPercentage05(): int
     {
-        return $this->costTransparencyPercentage05;
+        return $this->ensurePercentageRange($this->costTransparencyPercentage05);
     }
 
     public function setCostTransparencyPercentage05(?int $costTransparencyPercentage05): void
     {
-        $this->costTransparencyPercentage01 = $costTransparencyPercentage05;
+        $this->costTransparencyPercentage01 = $this->ensurePercentageRange($costTransparencyPercentage05);
+    }
+
+    protected function ensurePercentageRange(?int $value): int
+    {
+        if (empty($value) || $value < 0) {
+            return 0;
+        }
+
+        if ($value > 100) {
+            return 100;
+        }
+
+        return $value;
     }
 }
