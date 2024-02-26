f2d2dd21cece059124f479b8cf642a3293c47202
Ingo Steinke <ingosteinke@gmx.de>
2024-02-26 08:52:43 +0100

    define database scheme

--- src/IngoSCostTransparency/src/Migration/Migration1708703243IngoSCostTransparency.php
+++ src/IngoSCostTransparency/src/Migration/Migration1708703243IngoSCostTransparency.php
@@ -15,15 +15,19 @@ class Migration1708703243IngoSCostTransparency extends MigrationStep
     public function update(Connection $connection): void
     {
         $sql = <<<SQL
-CREATE TABLE IF NOT EXISTS `swag_example_extension` (
+CREATE TABLE IF NOT EXISTS `ingo_s_cost_transparency_extension` (
     `id` BINARY(16) NOT NULL,
     `product_id` BINARY(16) NULL,
-    `custom_string` VARCHAR(255) NULL,
+    `cost_transparency_percentage_01` VARCHAR(255) NULL,
+    `cost_transparency_percentage_02` VARCHAR(255) NULL,
+    `cost_transparency_percentage_03` VARCHAR(255) NULL,
+    `cost_transparency_percentage_04` VARCHAR(255) NULL,
+    `cost_transparency_percentage_05` VARCHAR(255) NULL,
     `created_at` DATETIME(3) NOT NULL,
     `updated_at` DATETIME(3) NULL,
     PRIMARY KEY (`id`),
-    KEY `fk.swag_example_extension.product_id` (`product_id`),
-    CONSTRAINT `fk.swag_example_extension.product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    KEY `fk.ingo_s_cost_transparency_extension.product_id` (`product_id`),
+    CONSTRAINT `fk.ingo_s_cost_transparency_extension.product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 SQL;
         $connection->executeStatement($sql);
