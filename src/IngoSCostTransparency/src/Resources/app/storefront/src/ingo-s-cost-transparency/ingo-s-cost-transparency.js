2f0a1245e37a0441a43e8ef46e1ab291c93e66c2
Ingo Steinke <ingosteinke@gmx.de>
2024-04-02 18:08:59 +0200

    fixup id assumption

--- src/IngoSCostTransparency/src/Resources/app/storefront/src/ingo-s-cost-transparency/ingo-s-cost-transparency.js
+++ src/IngoSCostTransparency/src/Resources/app/storefront/src/ingo-s-cost-transparency/ingo-s-cost-transparency.js
@@ -55,37 +55,57 @@ export default class IngoSCostTransparency extends Plugin {
         }
     }
     addEffectsAndSwitchDetails(event) {
+        /*
+        Nach Off-Canvas-Rendering können wir nicht mehr davon ausgehen, dass es unsere Elemente jeweils nur einmal git.
+        Somit verbietet es sich, mit document.getElementById oder analogen Selektoren zu arbeiten.
+        Also arbeiten wir entweder über die Plugin-Instanz (als Frontend-Entwickler bestimmt nicht!)
+        oder iterieren über alles passende, was wir mit getElementsByClassName finden?
+         */
+
         if (!event) { return; }
         console.log('addEffectsAndSwitchDetails(event), event:', event);
         const activeGroupElement = event.currentTarget;
         console.log(' activeGroupElement = event.currentTarget:', activeGroupElement);
         if (!activeGroupElement) { console.log('no activeGroupElement'); return; }
         activeGroupElement.classList.add('ingos-active');
-        const activeId = activeGroupElement.id;
-        const costGroupContentWrapper = document.getElementById('ingos-cost-group-contents');
-        console.log('costGroupContentWrapper:', costGroupContentWrapper);
-        if (!costGroupContentWrapper) { return; }
-        const activeContent = costGroupContentWrapper.querySelector('*[data-for="' +  activeId + '"]');
-        if (!activeContent) { return; }
-        activeContent.classList.add('ingos-active');
-        console.log('added className ingos-active to activeContent', activeContent);
+        const activeId = activeGroupElement.dataset.id;
+        const costGroupContentWrappers = document.getElementsByClassName('ingos-cost-group-contents');
+        console.log('costGroupContentWrappers:', costGroupContentWrappers);
+        for (let cgIndex = 0; cgIndex < costGroupContentWrappers.length; cgIndex++) {
+            let costGroupContentWrapper = costGroupContentWrappers.item(cgIndex);
+            const activeContent = costGroupContentWrapper.querySelector('*[data-for="' +  activeId + '"]');
+            if (!activeContent) { continue; }
+            activeContent.classList.add('ingos-active');
+            console.log('added className ingos-active to activeContent', activeContent);
 
-        const inactiveGroups = document.getElementsByClassName('ingos-cost-group');
-        for (let i = 0; i < inactiveGroups.length; i++) {
-            console.log(`try to compare if inactiveGroups.item(${i}) !== activeGroupElement`);
-            if (inactiveGroups.item(i) !== activeGroupElement) {
-                inactiveGroups.item(i).classList.remove('ingos-active');
+            // völlig unlesbare Benamung, die noch auf Code von Chris basiert :(((((
+
+            // wie hätte ich den den sinnvollerweise semantisch und selbsterklärend selbst benannt?
+            // was aber auch noch lange nicht erklärt, warum der bloß im offcanvas nicht funktioniert
+
+            // .ingos-cost .ingos-cost-group.ingos-cost-group-0x
+            // cost-group are the containers around the bar chart bars and their captions
+            const inactiveGroups = document.getElementsByClassName('ingos-cost-group');
+            for (let i = 0; i < inactiveGroups.length; i++) {
+                console.log(`try to compare if inactiveGroups.item(${i}) !== activeGroupElement`);
+                if (inactiveGroups.item(i) !== activeGroupElement) {
+                    inactiveGroups.item(i).classList.remove('ingos-active');
+                }
             }
-        }
-        let activeGroup = document.getElementsByClassName('ingos-cost-group-content');
-        console.log('activeGroup = .ingos-cost-group-content : ', activeGroup);
-        for (let i = 0; i < activeGroup.length; i++) {
-            console.log(`try to compare if activeGroup.item(${i}) !== activeContent`);
-            console.log('activeContent:', activeContent);
-            console.log(`activeGroup.item(${i})`, activeGroup.item(i));
-            // could there be a safer, more generic approach, comparing data or tokens instead of elements?
-            if (activeGroup.item(i) !== activeContent) {
-                activeGroup.item(i).classList.remove('ingos-active');
+
+            // .ingos-cost-group-contents .ingos-cost-group-content (ohne Eigenklasse, aber mit data-for=...)
+            // cost-group-content are the details description elements below the bar charts
+            let activeGroup = document.getElementsByClassName('ingos-cost-group-content');
+            console.log('activeGroup = .ingos-cost-group-content : ', activeGroup);
+            for (let i = 0; i < activeGroup.length; i++) {
+                console.log(`try to compare if activeGroup.item(${i}) !== activeContent`);
+                console.log('activeContent:', activeContent);
+                console.log(`activeGroup.item(${i})`, activeGroup.item(i));
+                // could there be a safer, more generic approach, comparing data or tokens instead of elements?
+                if (activeGroup.item(i) !== activeContent) {
+                    console.log('remove active class from this element',activeGroup.item(i) );
+                    activeGroup.item(i).classList.remove('ingos-active');
+                }
             }
         }
     }
