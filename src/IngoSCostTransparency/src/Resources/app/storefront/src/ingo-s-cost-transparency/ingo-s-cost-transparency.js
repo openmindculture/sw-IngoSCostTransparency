601b43e5ab6e81e09c748ea6a9f8a1db8cec9286
Ingo Steinke <ingosteinke@gmx.de>
2024-04-02 17:04:11 +0200

    handle off-canvas display (WIP)

--- src/IngoSCostTransparency/src/Resources/app/storefront/src/ingo-s-cost-transparency/ingo-s-cost-transparency.js
+++ src/IngoSCostTransparency/src/Resources/app/storefront/src/ingo-s-cost-transparency/ingo-s-cost-transparency.js
@@ -2,14 +2,50 @@ import Plugin from 'src/plugin-system/plugin.class';
 
 export default class IngoSCostTransparency extends Plugin {
     init() {
-        this.addAnimationEffectClassNames();
+        console.log('ready to set regular event handler');
+        this.addEffectsAndSwitchDetailsHandler();
+
+        console.log('ready to set percentage styles');
         this.applyPercentageStyles();
-    }
-    addAnimationEffectClassNames() {
-        const animatableElements = document.querySelectorAll('.ingos-cost-group');
-        for (let i=0; i < animatableElements.length; i++) {
-            animatableElements[i].addEventListener('click', this.costGroupEnterHandler);
+
+        // this.$emitter.publish('onClickOffCanvasTab');
+
+        console.log('done setting regular event handler, now add plugin emitter listener');
+        const tabElement = document.querySelector('#ingos-cost-transparency-tab');
+        if (tabElement) {
+            const offCanvasTabPlugin = window.PluginManager.getPluginInstanceFromElement(tabElement);
+            if (offCanvasTabPlugin) {
+                offCanvasTabPlugin.$emitter.subscribe('hideCookieBar', this.onHideCookieBar);
+            } else {
+                console.log('no offCanvasTabPlugin');
+            }
+        } else {
+            console.log('no tabElement');
         }
+
+        /*
+        if (tabElement && tabElement.$emitter && tabElement.$emitter.subscribe && typeof tabElement.$emitter.subscribe === 'function') {
+            tabElement.$emitter.subscribe('onClickOffCanvasTab', () => {
+                this.addEffectsAndSwitchDetailsHandler();
+            })
+        }
+        */
+    }
+    addEffectsAndSwitchDetailsHandler() {
+        console.log('addEffectsAndSwitchDetailsHandler');
+        const animatableElements = document.querySelectorAll('.ingos-cost-group');
+        console.log('animatableElements', animatableElements);
+        for (let i=0; i < animatableElements.length; i++) {
+            console.log(`animatableElements[${i}]`, animatableElements[i]);
+            if (!animatableElements[i].dataset.hasEventListener) {
+                console.log('Element had no event listener yet. Add event listener...')
+                animatableElements[i].addEventListener('click', this.addEffectsAndSwitchDetails);
+                animatableElements[i].dataset.hasEventListener='true';
+            } else {
+                console.log('element already has an event listener');
+            }
+        }
+        console.log('after iterating animatableElements');
     }
 
     applyPercentageStyles() {
@@ -24,7 +60,7 @@ export default class IngoSCostTransparency extends Plugin {
             }
         }
     }
-    costGroupEnterHandler(event) {
+    addEffectsAndSwitchDetails(event) {
         if (!event) { return; }
         const activeGroupElement = event.currentTarget;
         activeGroupElement.classList.add('ingos-active');
